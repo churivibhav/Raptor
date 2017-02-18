93062cb95beb095595cce7b42891d76136893bd4
Gparab <grcprb@gmail.com>
2017-02-18 15:08:56 +0530

    First commit

---
+++ src/main/java/com/base/test/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.base.test.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.base.test")
+public class AppConfig {
+
+}
